e1ceca48a42639f960dbe2bf72d28d2e9e86c471
Marvin Johanning <manimusmuellus@gmail.com>
2017-10-17 23:41:25 +0200

    Delete auth.rb

--- lib/auth.rb
+++
@@ -1,7 +0,0 @@
-require 'twitter'
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "HWR1tCHkc0rbyYqzQB4sCQc9x"
-  config.consumer_secret     = "05kftUArNyeHsAHGqrrtwrgJ4sqPVWJmqHroyx4VrQg8lxVVEn"
-  config.access_token        = "438658273-mVz3KP9ODjtnss5Hhat3t2St35kgbOBJNGSBKBSO"
-  config.access_token_secret = "yx1zIW9mQOvUOgHg0KN8pHQThe5umrrmCUjQIdMZBGVhU"
-end
